9f0b822d09f1ec09ff75e14b266469fa842ca0e2
hawai1401 <hawai1401@ik.me>
2025-12-26 22:15:48 +0100

    Ajout du système d'approbation pour les modifications

--- src/events/buttons/editDenied.ts
+++ src/events/buttons/editDenied.ts
@@ -1,7 +1,7 @@
 import { MessageFlags, type ButtonInteraction } from "discord.js";
 import type { botClient } from "../../index.js";
-import prisma from "../../db/prisma.js";
 import Container from "../../class/container.js";
+import prisma from "../../db/prisma.js";
 
 export const event = async (
   client: botClient,
@@ -9,37 +9,19 @@ export const event = async (
 ) => {
   await interaction.deferReply({ flags: MessageFlags.Ephemeral });
   const guildId = interaction.customId.split("_")[1]!;
-  const serveur = await prisma.serveur.findUnique({
-    where: { id: guildId },
-  });
   await prisma.serveur.update({
     where: { id: guildId },
     data: {
-      approuved: true,
-      description: serveur!.description_pending,
       description_pending: "",
-      badges: serveur!.badges_pending,
       badges_pending: [],
-      link: serveur!.link_pending,
       link_pending: "",
       pending: false,
     },
   });
   await interaction.message.delete();
-  if (interaction.customId.split("_")[2] === "edited")
   await interaction.editReply({
     components: [
-      new Container("success").addText(
-        `### :white_check_mark: - Modifications acceptées !`
-      ),
-    ],
-    flags: MessageFlags.IsComponentsV2,
-  });
-  await interaction.editReply({
-    components: [
-      new Container("success").addText(
-        `### :white_check_mark: - Serveur accepté !`
-      ),
+      new Container("error").addText(`### :x: - Modifications refusées !`),
     ],
     flags: MessageFlags.IsComponentsV2,
   });
