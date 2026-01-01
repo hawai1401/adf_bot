bf0eac3972d3ee0b25127bdcebea3b6b390e2359
hawai1401 <hawai1401@ik.me>
2026-01-01 22:29:18 +0100

    Ajout de la prise en charge de l'accélération Prisma et création de la commande pour vider le cache de la base de données

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,9 +1,8 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client/client.js";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
-export const prisma = new PrismaClient({ adapter: pool });
-
-const globalForPrisma = global as unknown as { prisma: typeof prisma };
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export const prisma = new PrismaClient({ adapter: pool }).$extends(
+  withAccelerate()
+);
